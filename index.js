c654514f3e0cb9697af16fe07b341f039e3123e4
donije09 <ustinedon@gmail.com>
2024-07-02 01:17:59 +0200

    movies names added
    middleware libray included

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-"console.log('Hello js');" 
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+
+app.use(morgan('combined'));
+app.get('/movies', (req,res)=> {
+    res.json({
+        movies: [
+            {title: 'inception', year:2010},
+            {title: 'spiderman', year:2012},
+            {title: 'Wakanda', year:2015},
+            {title: 'Daylight', year:2016},
+            {title: 'baby on board', year:2017},
+            {title: 'flight to Abuja', year:2018},
+            {title: 'monkey shadow', year:2019},
+            {title: 'mission impossible', year:2020},
+            {title: 'bad boys', year:2021},
+            {title: 'moremi', year:2022},
+        ]
+    });
+});
+app.get('/', (req,res) => {
+    res.send('welcome to the movie api');
+});
+app.use(express.static(path.join(__dirname, 'public')));
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('something broke');
+});
+const port = 8080;
+app.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`);
+});
+
