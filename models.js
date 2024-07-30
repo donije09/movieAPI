211d3c8076a45e59f341ffa7c7801e9c973fbea9
donije09 <ustinedon@gmail.com>
2024-07-30 12:02:58 +0200

    Set up MongoDB connection and dotenv

--- models.js
+++ models.js
@@ -2,29 +2,19 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }] // Optional: if you track favorite movies
+  favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
-// Method to hash the password
-userSchema.statics.hashPassword = function(password) {
-  return bcrypt.hashSync(password, 12);
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
 };
 
-// Method to validate the password
-userSchema.methods.validatePassword = function(password) {
-  return bcrypt.compareSync(password, this.password);
+userSchema.methods.validatePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
 };
 
 const User = mongoose.model('User', userSchema);
-
-// Movie model (assuming it exists)
-const movieSchema = new mongoose.Schema({
-  // Your movie schema definition here
-});
-
-const Movie = mongoose.model('Movie', movieSchema);
-
-module.exports = { User, Movie };
+module.exports = User;
