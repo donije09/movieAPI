c628b1802275e60a3d39b844e2b9c161fc1f6340
donije09 <ustinedon@gmail.com>
2024-08-01 19:53:02 +0200

    fix unauthorized issue

--- passport.js
+++ passport.js
@@ -25,15 +25,15 @@ passport.use(new LocalStrategy({
   }
 }));
 
-passport.use(new JwtStrategy({
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: jwtSecret
-}, (jwtPayload, done) => {
-  return User.findById(jwtPayload._id)
-    .then(user => {
-      return done(null, user);
-    })
-    .catch(err => {
-      return done(err);
-    });
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'your_jwt_secret'
+}, async (jwtPayload, callback) => {
+  return await Users.findById(jwtPayload._id)
+      .then((user) => {
+          return callback(null, user);
+      })
+      .catch((error) => {
+          return callback(error)
+      });
 }));
